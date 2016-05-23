456804be849ee0b81f299c2274dc89f1920a37dc
Sebastien N <snicolet@student.42.fr>
2016-05-23 20:39:50 +0200

    maj de check_cube pour agrandir une zone

--- check_cube.c
+++ check_cube.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/21 20:29:11 by snicolet          #+#    #+#             */
-/*   Updated: 2016/05/21 20:39:36 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/05/23 20:10:52 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	check_between(float a, float min, float max)
 	return ((a >= min) && (a <= max));
 }
 
-int			check_cube(t_box *a, t_box *b)
+int			check_cube(const t_box *a, const t_box *b)
 {
 	if (((check_between(a->zmin, b->zmin, b->zmax)) &&
 		(check_between(a->xmin, b->xmin, b->xmax)) &&
@@ -27,4 +27,24 @@ int			check_cube(t_box *a, t_box *b)
 		(check_between(a->ymax, b->ymin, b->ymax))))
 		return (1);
 	return (0);
+}
+
+/*
+** this function extens the box a to be able to contain the box b
+*/
+
+void		update_cube(t_box *a, const t_box *b)
+{
+	if (a->xmin > b->xmin)
+		a->xmin = b->xmin;
+	if (a->ymin > b->ymin)
+		a->ymin = b->ymin;
+	if (a->zmin > b->zmin)
+		a->zmin = b->zmin;
+	if (a->xmax < b->xmax)
+		a->xmax = b->xmax;
+	if (a->ymax < b->ymax)
+		a->ymax = b->ymax;
+	if (a->zmax < b->zmax)
+		a->zmax = b->zmax;
 }
