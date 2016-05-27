efd39c85a93596174e7a83004c47e1a24ec5abc8
Sebastien N <snicolet@student.42.fr>
2016-05-27 23:28:32 +0200

    deplcement des events dans event.c et gestion de l orientation de la camera

---
+++ events.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   events.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/27 23:17:22 by snicolet          #+#    #+#             */
+/*   Updated: 2016/05/27 23:18:37 by snicolet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+#include "draw.h"
+#include "libft.h"
+
+int				keydown(int keycode, t_rt *rt)
+{
+	(void)keycode;
+	(void)rt;
+	return (0);
+}
+
+int				keyrlz(int keycode, t_rt *rt)
+{
+	(void)keycode;
+	(void)rt;
+	return (0);
+}
+
+int				sdl_event(SDL_Event *event, t_rt *rt)
+{
+	if (event->type == SDL_QUIT)
+		return (1);
+	else if ((event->type == SDL_WINDOWEVENT) &&
+		(event->window.event == SDL_WINDOWEVENT_RESIZED))
+	{
+		rt->sys.geometry = draw_make_px(event->window.data1,
+			event->window.data2);
+		if (!(rt->sys.screen = SDL_GetWindowSurface(rt->sys.win)))
+			return (2);
+	}
+	else if (event->type == SDL_KEYDOWN)
+		return (keydown(event->key.keysym.sym, rt));
+	else if (event->type == SDL_KEYUP)
+		return (keyrlz(event->key.keysym.sym, rt));
+	return (0);
+}
