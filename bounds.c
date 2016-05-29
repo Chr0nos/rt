d222eafea1a7d03c6846a9e1c3a4dc598afc08f7
Sebastien N <snicolet@student.42.fr>
2016-05-29 04:57:39 +0200

    fix

--- bounds.c
+++ bounds.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/29 03:26:12 by snicolet          #+#    #+#             */
-/*   Updated: 2016/05/29 04:06:36 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/05/29 04:57:33 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 static void	rt_bounds_update_in(t_obj *obj, void *userdata)
 {
-	(void)obj;
 	(void)userdata;
 	update_cube(&obj->bounds, &obj->hitbox);
-	update_cube(&obj->parent->bounds, &obj->hitbox);
+	if (obj->parent)
+		update_cube(&obj->parent->bounds, &obj->hitbox);
 }
 
 void		rt_bounds_update(t_obj *node)
 {
-	rt_node_foreach(node, INFIX, &rt_bounds_update_in, NULL);
+	rt_node_foreach(node, SUFFIX, &rt_bounds_update_in, NULL);
 }
--- node.c
+++ node.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/29 03:31:46 by snicolet          #+#    #+#             */
-/*   Updated: 2016/05/29 04:39:59 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/05/29 04:55:15 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,19 @@ void	rt_node_display(t_obj *obj, void *userdata)
 
 void	rt_node_display_bounds(t_obj *obj, void *userdata)
 {
-	const t_box				*b = &obj->bounds;
-	char					*min;
-	char					*max;
-	unsigned int			p;
+	char				*x[6];
+	const unsigned int	p = 4;
 
 	rt_node_display(obj, userdata);
-	p = 3;
-	ft_putstr("\n\t");
-	while (p--)
-	{
-		min = ft_dtoa((double)b->xmin, 4);
-		max = ft_dtoa((double)b->xmax, 4);
-		ft_printf(" [%s:%s]", min, max);
-		ft_mfree(2, min, max);
-		b = (t_box*)(unsigned long)b + (sizeof(float) * 2);
-	}
-	ft_putchar('\n');
+	x[0] = ft_dtoa((double)obj->bounds.xmin, p);
+	x[1] = ft_dtoa((double)obj->bounds.xmax, p);
+	x[2] = ft_dtoa((double)obj->bounds.ymin, p);
+	x[3] = ft_dtoa((double)obj->bounds.ymax, p);
+	x[4] = ft_dtoa((double)obj->bounds.zmin, p);
+	x[5] = ft_dtoa((double)obj->bounds.zmax, p);
+	ft_printf("\n\t[%s:%s] [%s:%s] [%s:%s]\n", x[0], x[1], x[2], x[3],x[4],
+		x[5]);
+	ft_free_tab(x, 6);
 }
 
 void	rt_node_foreach(t_obj *node, int mode, void (*f)(t_obj*, void*),
