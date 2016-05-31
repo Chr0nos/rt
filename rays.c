d26385a76375e5e1beb4c05587ff67ff04982aa0
Sebastien N <snicolet@student.42.fr>
2016-05-31 18:29:46 +0200

    ajout d une fonction de debug de rayons

--- rays.c
+++ rays.c
@@ -6,16 +6,28 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/29 01:06:28 by snicolet          #+#    #+#             */
-/*   Updated: 2016/05/31 18:12:18 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/05/31 18:26:54 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
+#include "libft.h"
+#include <unistd.h>
+
+static void		rt_debug_ray(t_ray *ray)
+{
+	ft_putstr("start: ");
+	draw_putvector(ray->start, 3);
+	ft_putstr("\ndir: ");
+	draw_putvector(ray->dir, 3);
+	ft_putchar('\n');
+}
 
 static void		rt_rays_pixels(t_rt *rt, t_ray *ray, t_camera *camp)
 {
 	t_v2i			px;
 	t_mattf			m;
+	int				limit = 42;
 
 	px.x = rt->sys.geometry.x;
 	while (px.x--)
@@ -26,6 +38,8 @@ static void		rt_rays_pixels(t_rt *rt, t_ray *ray, t_camera *camp)
 		{
 			ray->dir = draw_vector_transform(ray->dir, &m);
 			rt_raycast(rt, ray);
+			if (!limit--)
+				exit(0);
 		}
 		m = draw_make_matrix_rot_y((double)camp->steppx.x);
 		ray->dir = draw_vector_transform(ray->dir, &m);
@@ -34,6 +48,7 @@ static void		rt_rays_pixels(t_rt *rt, t_ray *ray, t_camera *camp)
 
 t_uint			rt_raycast(t_rt *rt, t_ray *ray)
 {
+	rt_debug_ray(ray);
 	(void)rt;
 	(void)ray;
 	return (COLOR_BLACK);
