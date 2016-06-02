6479c77eabf3264f2561f7b078cca79678b8d316
Sebastien N <snicolet@student.42.fr>
2016-06-02 17:37:56 +0200

    netoyage

--- events.c
+++ events.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/27 23:17:22 by snicolet          #+#    #+#             */
-/*   Updated: 2016/06/02 16:18:45 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/06/02 17:36:59 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,18 @@ static void		display(t_rt *rt)
 	SDL_UpdateWindowSurface(rt->sys.win);
 }
 
-static void		rotate_camera(t_rt *rt, t_v3f deg)
-{
-	t_obj		*cam;
-
-	cam = rt->root->content;
-	(void)cam;
-	(void)deg;
-	//cam->trans.w = draw_vector_add(cam->trans.offset, deg);
-}
-
 int				keydown(int keycode, t_rt *rt)
 {
 	if (keycode == SDLK_ESCAPE)
 		return (1);
 	else if (keycode == SDLK_d)
-	{
 		((t_obj*)rt->root->content)->trans.w.x += 0.5;
-		(void)rotate_camera;
-		//rotate_camera(rt, (t_v3f){0.1f, 0.0f, 0.0f});
-	}
 	else if (keycode == SDLK_a)
 		((t_obj*)rt->root->content)->trans.w.x -= 0.5;
-	else if (keycode == SDLK_o)
-	{
-		ft_putstr("camera offset -> ");
-		draw_putv4d(((t_obj*)rt->root->content)->trans.w, 6);
-		ft_putchar('\n');
-	}
+	else if (keycode == SDLK_w)
+		((t_obj*)rt->root->content)->trans.w.y += 0.5;
+	else if (keycode == SDLK_s)
+		((t_obj*)rt->root->content)->trans.w.y -= 0.5;
 	display(rt);
 	return (0);
 }
