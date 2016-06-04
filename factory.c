773e79e6d85a9091895a35d2ca5164508eddebdb
Sebastien N <snicolet@student.42.fr>
2016-06-05 00:21:46 +0200

    mise a la norme de factory

--- factory.c
+++ factory.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/28 00:08:40 by snicolet          #+#    #+#             */
-/*   Updated: 2016/06/05 00:06:36 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/06/05 00:21:11 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 #include "libft.h"
 #include <stdlib.h>
 
-t_uint				rt_sizeof(t_type type)
+unsigned int		rt_sizeobj(t_type type)
 {
 	unsigned int		p;
-	t_typesize			sizes[4];
+	const t_typesize	sizes[] = {
+		(t_typesize){CUBE, sizeof(struct s_cube)},
+		(t_typesize){PLAN, sizeof(struct s_plan)},
+		(t_typesize){SPHERE, sizeof(struct s_sphere)},
+		(t_typesize){CAMERA, sizeof(struct s_camera)}
+	};
 
-	p = 0;
-	sizes[p++] = (t_typesize){CUBE, sizeof(t_cube)};
-	sizes[p++] = (t_typesize){PLAN, sizeof(t_plan)};
-	sizes[p++] = (t_typesize){SPHERE, sizeof(t_sphere)};
-	sizes[p++] = (t_typesize){CAMERA, sizeof(t_camera)};
+	p = 4;
 	while (p--)
 		if (sizes[p].type == type)
 			return (sizeof(t_obj) + (unsigned int)sizes[p].size);
@@ -33,7 +34,7 @@ t_uint				rt_sizeof(t_type type)
 t_obj				*rt_factory_alloc(t_type type, t_obj *parent)
 {
 	static t_uint		lastid = 1;
-	const t_uint		size = rt_sizeof(type);
+	const t_uint		size = rt_sizeobj(type);
 	t_obj				*obj;
 
 	if (type == ROOT)
