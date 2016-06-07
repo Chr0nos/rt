4aabbc556fbbfae3d4bceda8b031490ad8c2fa82
Quentin Loubiere <qloubier@student.42.fr>
2016-06-07 20:19:31 +0200

    change camera raycast

--- rays.c
+++ rays.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rays.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/29 01:06:28 by snicolet          #+#    #+#             */
-/*   Updated: 2016/06/05 21:13:20 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/06/07 20:15:08 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,9 @@
 #include "libft.h"
 #include <unistd.h>
 
-static void		rt_rayplan_fix(t_ray *ray, t_camera *camp, t_v2d *pfix, t_m4 *m)
+static void		rt_rayplan_fix(t_ray *ray, t_m4 *m)
 {
-	t_v2d		tmp;
-
-	tmp = (t_v2d){fabs(pfix->x), fabs(pfix->y)};
 	ray->dir = draw_vector_transform_m4(ray->dir, m);
-	ray->start = (t_v4d){
-		camp->raypos.x + (camp->mdirx.x * tmp.x) + (camp->mdiry.x * tmp.y),
-		camp->raypos.y + (camp->mdirx.y * tmp.x) + (camp->mdiry.y * tmp.y),
-		camp->raypos.z + (camp->mdirx.z * tmp.x) + (camp->mdiry.z * tmp.y),
-		1.0};
-	pfix->y -= camp->rayfix.w;
 }
 
 static void		rt_debug_ray(t_ray *ray)
@@ -41,35 +32,28 @@ static void		rt_debug_ray(t_ray *ray)
 		exit(0);
 }
 
-static void		rt_rays_pixels(t_rt *rt, t_ray *ray, t_camera *camp)
+static void		rt_rays_pixels(t_rt *rt, t_ray *ray, t_camera *camp, t_m4 m)
 {
 	t_v2i			px;
-	t_v2d			pfix;
-	t_m4			m;
+	t_v4d			rad;
 
 	(void)rt_debug_ray;
 	px.x = rt->sys.geometry.x;
-	pfix.x = camp->rayfix.x;
+	rad = (t_v4d){camp->rayreset.x, 0.0, camp->steppx.x, camp->steppx.y};
 	while (px.x--)
 	{
-		m = draw_make_matrix_m4_x((t_v4d){0.0, 0.0, 0.0, 0.0},
-			-camp->steppx.x, (t_v4d){1.0, 1.0, 1.0, 1.0});
 		px.y = rt->sys.geometry.y;
-		pfix.y = camp->rayfix.y;
+		rad.y = camp->rayreset.y;
 		while (px.y--)
 		{
-			// ray->dir = draw_vector_transform_m4(ray->dir, &m);
-			rt_rayplan_fix(ray, camp, &pfix, &m);
+			ray->dir = (t_v4d){sin(rad.y), -sin(rad.x),
+				cos(rad.x) * cos(rad.y), 0.0};
+			rt_rayplan_fix(ray, &m);
 			draw_pxi(rt->sys.screen->pixels, px,
 				(unsigned int)rt->sys.geometry.x, rt_render(rt, ray));
+			rad.y -= rad.w;
 		}
-		m = draw_make_matrix_m4_y((t_v4d){0.0, 0.0, 0.0, 0.0},
-			-camp->steppx.x, (t_v4d){1.0, 1.0, 1.0, 1.0});
-		ray->dir = draw_vector_transform_m4(ray->dir, &m);
-		m = draw_make_matrix_m4_x((t_v4d){0.0, 0.0, 0.0, 0.0},
-			camp->rayreset.x * 2.0, (t_v4d){1.0, 1.0, 1.0, 1.0});
-		ray->dir = draw_vector_transform_m4(ray->dir, &m);
-		pfix.x -= camp->rayfix.z;
+		rad.x -= rad.z;
 	}
 }
 
@@ -78,7 +62,6 @@ void			rt_rays(t_rt *rt)
 	t_obj		*cam;
 	t_camera	*camp;
 	t_ray		ray;
-	t_mattf		m;
 
 	cam = (t_obj*)rt->root->content;
 	camp = cam->content;
@@ -86,17 +69,7 @@ void			rt_rays(t_rt *rt)
 	ray.start = cam->trans.w;
 	camp->raypos = cam->trans.w;
 	ray.limit = 0.0;
-	m = draw_matrix_multiply_axes_m4(camp->rayreset,
-		(t_v4d){1.0, 1.0, 1.0, 1.0},
-		(t_v4d){0.0, 0.0, 0.0, 0.0});
-	ray.dir = draw_matrix_multiply_m4(
-		draw_matrix_multiply_m4((t_v4d){0.0, 0.0, 1.0, 0.0}, &cam->trans), &m);
-	m = draw_make_matrix_m4_x((t_v4d){0.0, 0.0, 0.0, 0.0},
-		camp->rayreset.x, (t_v4d){1.0, 1.0, 1.0, 1.0});
-	camp->mdiry = draw_matrix_multiply_m4(cam->trans.z, &m);
-	m = draw_make_matrix_m4_y((t_v4d){0.0, 0.0, 0.0, 0.0},
-		camp->rayreset.y, (t_v4d){1.0, 1.0, 1.0, 1.0});
-	camp->mdirx = draw_matrix_multiply_m4(cam->trans.z, &m);
-	rt_rays_pixels(rt, &ray, camp);
+	ray.dir = (t_v4d){0.0, 0.0, 1.0, 0.0};
+	rt_rays_pixels(rt, &ray, camp, cam->trans);
 	(void)rt_rays_pixels;
 }
