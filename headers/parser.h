ff6bf008ce14836e799a878372b54cf39c820fb1
Diana BOUDY <dboudy@e2r6p6.42.fr>
2016-06-15 10:29:25 +0200

    new train avec des rails :D, et correction parser cyl

--- headers/parser.h
+++ headers/parser.h
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/10 17:55:23 by snicolet          #+#    #+#             */
-/*   Updated: 2016/06/13 16:08:30 by dboudy           ###   ########.fr       */
+/*   Updated: 2016/06/15 09:38:51 by dboudy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@
 # define PLAN_OR_Z 5
 # define PLAN_COLOR 6
 # define CONE_COLOR 4
-# define CYL_COLOR_POS 7
-# define CYL_OR_X 4
-# define CYL_OR_Y 5
-# define CYL_OR_Z 6
+# define CYL_COLOR_POS 4
+# define CYL_OR_X 5
+# define CYL_OR_Y 6
+# define CYL_OR_Z 7
 # define CYL_HEIGHT 8
 # define MINFOV 5.0
 # define MAXFOV 180.0
