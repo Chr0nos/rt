0826892cfa98f3fe19a0f83bda562577cdccc74b
Sebastien N <snicolet@student.42.fr>
2016-06-21 20:10:24 +0200

    nombre de miniatures suivant la taille de la finetre

--- events/events.c
+++ events/events.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/27 23:17:22 by snicolet          #+#    #+#             */
-/*   Updated: 2016/06/21 18:10:16 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/06/21 20:09:48 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,8 @@ int				sdl_event(SDL_Event *event, t_rt *rt)
 			event->window.data2);
 		if (!(rt->sys.screen = SDL_GetWindowSurface(rt->sys.win)))
 			return (2);
+		rt->menu.items.x = rt->sys.geometry.x / (rt->menu.thumb.x + 5);
+		rt->menu.items.y = rt->sys.geometry.y / (rt->menu.thumb.y + 5);
 		rt->keyboard |= FORCE_DISPLAY;
 	}
 	else if (event->type == SDL_KEYDOWN)
