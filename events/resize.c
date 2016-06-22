e7d399d1ae4300069d82b7d79b93c68a2638ad40
Sebastien N <snicolet@student.42.fr>
2016-06-22 16:11:54 +0200

    amelioration de la detection du nombre de vignetes a la fenetre

--- events/resize.c
+++ events/resize.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/21 21:19:40 by snicolet          #+#    #+#             */
-/*   Updated: 2016/06/21 21:20:34 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/06/22 16:11:20 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int		rt_event_resize(SDL_Event *event, t_rt *rt)
 		event->window.data2);
 	if (!(rt->sys.screen = SDL_GetWindowSurface(rt->sys.win)))
 		return (2);
-	rt->menu.items.x = rt->sys.geometry.x / (rt->menu.thumb.x + 5);
-	rt->menu.items.y = rt->sys.geometry.y / (rt->menu.thumb.y + 5);
+	rt->menu.items.x = rt->sys.geometry.x / (rt->menu.thumb.x + 10);
+	rt->menu.items.y = rt->sys.geometry.y / (rt->menu.thumb.y + 10);
 	rt->keyboard |= FORCE_DISPLAY;
 	return (0);
 }
