ee38f56651e732860bb9dfcbaeee60c57f831d30
Sebastien N <snicolet@student.42.fr>
2016-06-23 17:58:45 +0200

    ajout du proto pour le filtre sepia

--- headers/filter.h
+++ headers/filter.h
@@ -6,7 +6,7 @@
 /*   By: dboudy <dboudy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/23 11:16:20 by dboudy            #+#    #+#             */
-/*   Updated: 2016/06/23 16:33:36 by dboudy           ###   ########.fr       */
+/*   Updated: 2016/06/23 17:58:31 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ t_uint          filter_blue(t_uint color);
 t_uint          filter_magenta(t_uint color);
 t_uint          filter_yellow(t_uint color);
 t_uint          filter_cyan(t_uint color);
+t_uint			filter_sepia(t_uint	c);
 
 #endif
