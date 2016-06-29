9db1b7c2878aca908ff3487feec3f0eced66263c
Sebastien N <snicolet@student.42.fr>
2016-06-29 22:21:44 +0200

    fix du fov en sda defaults

---
+++ parser/sda/sda_defaults.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sda_defaults.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/29 21:24:11 by snicolet          #+#    #+#             */
+/*   Updated: 2016/06/29 22:19:46 by snicolet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+#include "sda.h"
+#include "libft.h"
+#include "unistd.h"
+
+static void sda_set_camera_default(t_obj *obj)
+{
+	t_camera	*cam;
+
+	cam = obj->content;
+	if (!(obj->cfgbits & SDB_FOV))
+		cam->fov = -49.124;
+}
+
+static int 	sda_set_obj_defaults(t_obj *obj, int mode, void *userdata)
+{
+	(void)mode;
+	(void)userdata;
+	if (obj->type == CAMERA)
+		sda_set_camera_default(obj);
+	return (OK);
+}
+
+void		sda_set_defaults(t_obj *root)
+{
+	rt_node_foreach(root, PREFIX, &sda_set_obj_defaults, NULL);
+}
