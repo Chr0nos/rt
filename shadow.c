5f91a81b586729c7cdf4dd38165681d407cf51c5
Hantlowt <lhote.antoine@gmail.com>
2016-07-12 11:58:26 +0200

    Fix some shadows problem

--- shadow.c
+++ shadow.c
@@ -20,9 +20,9 @@ void			shader_shadow(t_shader *s, t_render *r, t_obj *light)
 	t_render	sw;
 
 	ray = *r->ray;
-	ray.dir = geo_subv4(light->trans.w, r->intersection);
 	ray.start = geo_addv4(r->intersection, geo_multv4(ray.dir,
-		geo_dtov4d(0.00001)));
+		geo_dtov4d(-0.00001)));
+	ray.dir = geo_subv4(light->trans.w, r->intersection);
 	ray.lenght = (double)INFINITY;
 	sw = (t_render){
 			&ray, r->rt,
