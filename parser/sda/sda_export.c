9b801b85fac2e7d067ec1930ee285a7d6b7319c3
Sébastien Nicolet <snicolet@student.42.fr>
2016-07-16 02:26:46 +0200

    rustine

--- parser/sda/sda_export.c
+++ parser/sda/sda_export.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/15 13:25:55 by snicolet          #+#    #+#             */
-/*   Updated: 2016/07/16 02:06:24 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/07/16 02:26:21 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ static void	sda_export_settings(const t_rt *rt)
 		10, "0123456789");
 	sda_export_color_raw(rt->settings.default_color, color_str);
 	sda_export_color_raw(rt->settings.bgcolor, background_str);
-	al = ft_dtoa(rt->settings.ambiant_light, 6);
+	al = ft_dtoa(rt->settings.ambiant_light * 100.0, 6);
 	ft_printf("SETTING\n\tcolor: %s\n\treflect: %s\n\tal: %s\n",
 		color_str, reflect_str, al);
 	ft_printf("\tbackground: %s\n", background_str);
