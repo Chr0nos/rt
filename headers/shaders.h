6ed1521e0911bcd9ae7fe96eca05ac750b9285b2
Quentin Loubiere <qloubier@student.42.fr>
2016-07-18 19:10:47 +0200

    new shader type

--- headers/shaders.h
+++ headers/shaders.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   shaders.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
+/*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/21 14:34:04 by alhote            #+#    #+#             */
-/*   Updated: 2016/07/17 21:28:53 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/07/18 17:28:11 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,28 @@
 # define R(C) ((C & 0x00FF0000) >> 16)
 # define G(C) ((C & 0x0000FF00) >> 8)
 # define B(C) (C & 0x000000FF)
+# define SHT t_shatyp
+# define SHF t_shafnc
+
+typedef enum		e_shader_type
+{
+	NO_SHADER = 0,
+	LIGHT_SHADER = 1,
+	INTERSECT_SHADER = 1 << 1,
+	SCREEN_SHADER = 1 << 2
+}					t_shatyp;
+
+typedef enum		e_shader_func
+{
+	NONE = 0,
+	EXEC = 1,
+	BLEND = 2,
+	INTERSECT = 3,
+}					t_shafnc;
 
 typedef struct		s_shader
 {
+	void			*(*sh_get)(struct s_shader *, SHT, SHF);
 	int				enabled;
 	unsigned int	color_render;
 	unsigned int	color_base;
