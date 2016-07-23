5b2662e1359338d7afdd7a3a6a56d98aa3487c3b
Chr0nos <snicolet@student.42.fr>
2016-07-23 13:33:29 +0000

    fix compil linux

--- signal.c
+++ signal.c
@@ -12,6 +12,9 @@
 
 #include "rt.h"
 #include "libft.h"
+#ifndef SIGUSR1
+# define SIGUSR1 30
+#endif
 
 void		rt_signal_singletone(t_point *geometry, t_point *px, char delete)
 {
@@ -32,7 +35,7 @@ void		rt_signal_singletone(t_point *geometry, t_point *px, char delete)
 	else if ((!pts[0]) || (!pts[1]))
 		return ;
 	pc = rt_rays_pc(*pts[0], *pts[1]);
-	ft_printf("status: %d%%\n", (int)pc);
+	//ft_printf("status: %d%%\n", (int)pc);
 }
 
 void		rt_signal(int num)
