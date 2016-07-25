ae9d47b0ba3335783bda569a5505b8af64e40a7a
Sebastien N <snicolet@student.42.fr>
2016-07-25 14:28:45 +0200

    optimisation des shaders texture et normale

--- shaders/shader_normalmap.c
+++ shaders/shader_normalmap.c
@@ -6,7 +6,7 @@
 /*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/16 15:27:09 by alhote            #+#    #+#             */
-/*   Updated: 2016/07/20 12:14:16 by dboudy           ###   ########.fr       */
+/*   Updated: 2016/07/25 14:26:51 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,31 +19,27 @@
 
 unsigned int			shader_color_normal_intersection(const t_render *r)
 {
-	t_texture		*tex;
-	unsigned int	*pixels_texture;
-	double			u;
-	double			v;
+	const t_texture		*tex = rt_obj_get_normal(r->obj_intersect);
+	const unsigned int	*pixels_texture = (tex) ? tex->surface->pixels : NULL;
+	double				u;
+	double				v;
 
-	tex = rt_obj_get_normal(r->obj_intersect);
-	u = 0.0;
-	v = 0.0;
-	if (tex)
+	if (!pixels_texture)
+		return (0);
+	if (r->obj_intersect->type & SPHERE)
 	{
-		pixels_texture = tex->surface->pixels;
-		u = fabs((double)((int)((fabs((r->normal.x != 0.0 ? r->intersection.z :
-			r->intersection.x)) / 1.0) * 1000.0) % 1000) / 1000.0);
-		v = fabs((double)((int)((fabs((r->normal.y != 0.0 ? r->intersection.z :
-			r->intersection.y)) / 1.0) * 1000.0) % 1000) / 1000.0);
-		if (r->obj_intersect->type & SPHERE)
-		{
-			u = 0.5 + (atan2(r->normal.z, r->normal.x) / (2 * M_PI));
-			v = 0.5 - (asin(r->normal.y) / M_PI);
-		}
-		return (pixels_texture[tex->surface->w *
-			(int)(v * tex->surface->h) + (int)(u * tex->surface->w)]);
+		u = 0.5 + (atan2(r->normal.z, r->normal.x) / (2 * M_PI));
+		v = 0.5 - (asin(r->normal.y) / M_PI);
 	}
 	else
-		return (0);
+	{
+		u = fabs((double)((int)((fabs((r->normal.x != 0.0 ? r->intersection.z :
+			r->intersection.x)) * 1.0) * 1000.0) % 1000) * 0.001);
+		v = fabs((double)((int)((fabs((r->normal.y != 0.0 ? r->intersection.z :
+			r->intersection.y)) * 1.0) * 1000.0) % 1000) * 0.001);
+	}
+	return (pixels_texture[tex->surface->w *
+		(int)(v * tex->surface->h) + (int)(u * tex->surface->w)]);
 }
 
 void					shader_normalmap(t_shader *s, t_render *r, t_obj *light)
--- shaders/shader_texture.c
+++ shaders/shader_texture.c
@@ -6,7 +6,7 @@
 /*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/14 18:34:04 by alhote            #+#    #+#             */
-/*   Updated: 2016/07/18 17:39:09 by alhote           ###   ########.fr       */
+/*   Updated: 2016/07/25 14:24:16 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,27 @@
 
 unsigned int		shader_color_texture_intersection(const t_render *r)
 {
-	t_texture		*tex;
-	unsigned int	*pixels_texture;
-	double			u;
-	double			v;
+	const t_texture		*tex = rt_obj_get_texture(r->obj_intersect);
+	const unsigned int	*pixels_texture = (tex) ? tex->surface->pixels : NULL;
+	double				u;
+	double				v;
 
-	tex = rt_obj_get_texture(r->obj_intersect);
-	u = 0.0;
-	v = 0.0;
-	if (tex)
+	if (!pixels_texture)
+		return (0);
+	if (r->obj_intersect->type & SPHERE)
 	{
-		pixels_texture = tex->surface->pixels;
-		u = fabs((double)((int)(((r->normal.x != 0.0 ? r->intersection.z :
-			r->intersection.x) / 1.0) * 1000.0) % 1000) / 1000.0);
-		v = fabs((double)((int)(((r->normal.y != 0.0 ? r->intersection.z :
-			r->intersection.y) / 1.0) * 1000.0) % 1000) / 1000.0);
-		if (r->obj_intersect->type & SPHERE)
-		{
-			u = 0.5 + (atan2(r->normal.z, r->normal.x) / (2.0 * M_PI));
-			v = 0.5 - (asin(r->normal.y) / M_PI);
-		}
-		return (pixels_texture[tex->surface->w *
-			(int)(v * tex->surface->h) + (int)(u * tex->surface->w)]);
+		u = 0.5 + (atan2(r->normal.z, r->normal.x) / (2.0 * M_PI));
+		v = 0.5 - (asin(r->normal.y) / M_PI);
 	}
-	return (0);
+	else
+	{
+		u = fabs((double)((int)(((r->normal.x != 0.0 ? r->intersection.z :
+			r->intersection.x) * 1.0) * 1000.0) % 1000) * 0.001);
+		v = fabs((double)((int)(((r->normal.y != 0.0 ? r->intersection.z :
+			r->intersection.y) * 1.0) * 1000.0) % 1000) * 0.001);
+	}
+	return (pixels_texture[tex->surface->w *
+		(int)(v * tex->surface->h) + (int)(u * tex->surface->w)]);
 }
 
 void				shader_texture(t_shader *s, t_render *r, t_obj *light)
