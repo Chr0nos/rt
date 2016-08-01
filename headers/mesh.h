3624f7e1f10ff68ae45333c7ba4367705968e97f
Hantlowt <lhote.antoine@gmail.com>
2016-08-01 18:07:17 +0200

    update

--- headers/mesh.h
+++ headers/mesh.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mesh.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hantlowt <hantlowt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/08/01 17:51:04 by hantlowt          #+#    #+#             */
-/*   Updated: 2016/08/01 17:57:47 by hantlowt         ###   ########.fr       */
+/*   Created: 2016/08/01 17:51:04 by alhote            #+#    #+#             */
+/*   Updated: 2016/08/01 18:07:08 by alhote           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ typedef struct			s_vertex
 
 typedef struct			s_triangle
 {
+	unsigned int		color;
 	t_vertex			v1;
 	t_vertex			v2;
 	t_vertex			v3;
-	unsigned int		color;
 }						t_triangle;
 
 #endif
