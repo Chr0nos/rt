e8aa8c80d800a184abc50b56ad234d592375bc13
Diana BOUDY <dboudy@e1r6p14.42.fr>
2016-08-16 10:34:01 +0200

    norme display help

--- display.c
+++ display.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/04 23:21:50 by snicolet          #+#    #+#             */
-/*   Updated: 2016/07/30 03:22:56 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/08/16 10:32:59 by dboudy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 #include "keyboard.h"
 #include "menu.h"
 
-void	display_help_arg(void)
+void			display_help_arg(void)
 {
 	ft_putstr(
-	"\033[01;032m\n\
-	  _____________________________________________________________________\n\
-	 //                                                                   \\\\\n\
-	||\033[01;035m                        WELCOME IN OUR RT MENU\
-	               \033[01;032m||\n\
-	 \\\\___________________________________________________________________//\n\
-	|                                                                       |\n\
-	|  \033[04;036mNext time, add map and try option :\033[;032m\
-	                                |\n\
-	|  -e  : output scene to sda format.                                    |\n\
-	|  -b  : export scene to image bmp and choose a destination path        |\n\
-	|        -> ./rt -e scenes/test.sda ~Desktop/my_image.bmp               |\n\
-	|  -s  : define your map size.                                          |\n\
-	|        -> ./rt -s 800x800 scenes/test.sda                             |\n\
-	|  -fs : full screen mode.                                              |\n\
-	|  -x  : disable refresh mode.                                          |\n\
-	|_______________________________________________________________________|\n\
-	|________________________________  ENJOY _______________________________|\
-	\n\n\033[;m\
-	");
+		"\033[01;032m\n\
+		..______________________________________________________________..\n\
+		||                                                              ||\n\
+		||\033[01;035m                    WELCOME IN OUR RT MENU\
+		\033[01;032m        ||\n\
+		\\\\______________________________________________________________//\n\
+		|                                                                |\n\
+		|\
+		\033[04;036mNext time, add map and try option :\
+		\033[;032m |\n\
+		|  -e  : output scene to sda format.                             |\n\
+		|  -b  : export scene to image bmp & choose a destination path   |\n\
+		|        -> ./rt -e scenes/test.sda ~Desktop/my_image.bmp        |\n\
+		|  -s  : define your map size.                                   |\n\
+		|        -> ./rt -s 800x800 scenes/test.sda                      |\n\
+		|  -fs : full screen mode.                                       |\n\
+		|  -x  : disable refresh mode.                                   |\n\
+		|________________________________________________________________|\n\
+		|____________________________  ENJOY ____________________________|\
+		\n\n\033[;m");
 }
 
 int				sdl_flush(t_rt *rt)
