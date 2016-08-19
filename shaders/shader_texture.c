818dbcada7cd860f0276bd3115b054d51ca32f0e
Hantlowt <lhote.antoine@gmail.com>
2016-08-19 15:36:26 +0200

    Fix texture uv triangle

--- shaders/shader_texture.c
+++ shaders/shader_texture.c
@@ -6,7 +6,7 @@
 /*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/14 18:34:04 by alhote            #+#    #+#             */
-/*   Updated: 2016/08/18 21:17:49 by alhote           ###   ########.fr       */
+/*   Updated: 2016/08/19 15:18:04 by alhote           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,23 @@ static t_v2f		get_uv_triangle(t_obj *obj, t_v4d i)
 	t_v4d	f1;
 	t_v4d	f2;
 	t_v4d	f3;
+	double	area;
 	t_v4d	bary;
 
+	area = fabs(geo_dotv4(geo_crossv4(
+		geo_subv4(((t_triangle*)(obj->content))->v1.pos,
+		((t_triangle*)(obj->content))->v2.pos),
+		geo_subv4(((t_triangle*)(obj->content))->v3.pos,
+		((t_triangle*)(obj->content))->v1.pos)), rt_triangle_normale(obj, &i)));
 	f1 = geo_subv4(((t_triangle*)(obj->content))->v1.pos, i);
 	f2 = geo_subv4(((t_triangle*)(obj->content))->v2.pos, i);
 	f3 = geo_subv4(((t_triangle*)(obj->content))->v3.pos, i);
-
-	bary.x = geo_lenv4(geo_crossv4(f2, f3)) / geo_lenv4(rt_triangle_normale(obj, &i));
-	bary.y = geo_lenv4(geo_crossv4(f3, f1)) / geo_lenv4(rt_triangle_normale(obj, &i));
-	bary.z = geo_lenv4(geo_crossv4(f1, f2)) / geo_lenv4(rt_triangle_normale(obj, &i));
-	bary = geo_normv4(bary);
-	// bary.x = (double)geo_clamp((float)bary.x, 0.0, 1.0);
-	// bary.y = (double)geo_clamp((float)bary.y, 0.0, 1.0);
-	// bary.z = (double)geo_clamp((float)bary.z, 0.0, 1.0);
+	bary.x = fabs(geo_dotv4(geo_crossv4(f2, f3),
+	rt_triangle_normale(obj, &i))) / area;
+	bary.y = fabs(geo_dotv4(geo_crossv4(f3, f1),
+	rt_triangle_normale(obj, &i))) / area;
+	bary.z = fabs(geo_dotv4(geo_crossv4(f1, f2),
+	rt_triangle_normale(obj, &i))) / area;
 	return ((t_v2f){
 		(float)(bary.x * (double)((t_triangle*)(obj->content))->v1.uv.x + bary.y *
 		(double)((t_triangle*)(obj->content))->v2.uv.x + bary.z * (double)((t_triangle*)(obj->content))->v3.uv.x),
