d34ef9fc208b7a7861aecec5a69f3fa27602b64c
Sébastien Nicolet <snicolet@student.42.fr>
2016-08-28 16:04:32 +0200

    changement des touches pour l ambiant light

--- events/keybit.c
+++ events/keybit.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/09 17:42:39 by snicolet          #+#    #+#             */
-/*   Updated: 2016/06/30 22:43:18 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/08/28 16:03:39 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int			getkeybit(const int keycode)
 		{KC_RIGHT, ROTATE_RIGHT},
 		{KC_P, FORCE_DISPLAY},
 		{KC_LSHIFT, FAST},
-		{SDLK_m, AMBIANT_MORE | FORCE_DISPLAY},
-		{SDLK_n, AMBIANT_LESS | FORCE_DISPLAY}
+		{SDLK_n, AMBIANT_MORE | FORCE_DISPLAY},
+		{SDLK_h, AMBIANT_LESS | FORCE_DISPLAY}
 	};
 	unsigned int	p;
 
