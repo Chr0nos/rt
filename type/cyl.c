85da0534243dc112eb4e8134c2784e6db7014038
Sebastien NICOLET <snicolet@student.42.fr>
2016-08-31 04:57:49 +0200

    opti des cylindres: retrait de 3 divisions

--- type/cyl.c
+++ type/cyl.c
@@ -6,7 +6,7 @@
 /*   By: dboudy <dboudy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/13 10:45:12 by dboudy            #+#    #+#             */
-/*   Updated: 2016/08/31 04:55:31 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/08/31 04:57:26 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,13 +49,13 @@ int				rt_cyl_inter(t_obj *obj, t_ray *r, t_intersect *v)
 	const double	radius = (double)((t_cyl*)obj->content)->radius;
 	const t_v4d		x = geo_subv4(r->start, obj->trans.w);
 
-	tmp[0] = geo_dotv4(obj->rotation, obj->rotation);
+	tmp[0] = 1.0 / geo_dotv4(obj->rotation, obj->rotation);
 	tmp[1] = geo_dotv4(r->dir, obj->rotation);
 	tmp[2] = geo_dotv4(x, x);
 	tmp[3] = geo_dotv4(obj->rotation, x);
-	s.a = geo_dotv4(r->dir, r->dir) - (tmp[1] * tmp[1] / tmp[0]);
-	s.b = 2.0 * geo_dotv4(r->dir, x) - (2 * tmp[1] * tmp[3] / tmp[0]);
-	s.c = tmp[2] - (radius * radius) - ((tmp[3] * tmp[3]) / tmp[0]);
+	s.a = geo_dotv4(r->dir, r->dir) - (tmp[1] * tmp[1] * tmp[0]);
+	s.b = 2.0 * geo_dotv4(r->dir, x) - (2 * tmp[1] * tmp[3] * tmp[0]);
+	s.c = tmp[2] - (radius * radius) - ((tmp[3] * tmp[3]) * tmp[0]);
 	return (rt_cyl_solve(&s, r, &v->in));
 }
 
