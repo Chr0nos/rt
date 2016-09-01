f9ff48fe06259a090735053fe5d172a741718b49
edelangh <edelangh@student.42.fr>
2016-09-01 18:05:55 +0200

    norm render.c

--- render/render.c
+++ render/render.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/04 19:04:06 by snicolet          #+#    #+#             */
-/*   Updated: 2016/08/31 17:11:49 by edelangh         ###   ########.fr       */
+/*   Updated: 2016/09/01 18:04:11 by edelangh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static unsigned int	get_background_color(t_render *r)
 	u = 0.5 + (atan2(r->ray->dir.z, r->ray->dir.x) / (2.0 * M_PI));
 	v = 0.5 - (asin(r->ray->dir.y) / M_PI);
 	return (pixels_texture[tex->surface->w *
-		(int)(v * tex->surface->h) + (int)(u * tex->surface->w)]);
+			(int)(v * tex->surface->h) + (int)(u * tex->surface->w)]);
 }
 
 int					rt_render_foreach(t_obj *obj, int mode, void *userdata)
@@ -58,6 +58,19 @@ int					rt_render_foreach(t_obj *obj, int mode, void *userdata)
 	return (OK);
 }
 
+static void			init_colors(unsigned int *colors, t_shader *shader)
+{
+	int				i;
+
+	i = 0;
+	while (shader)
+	{
+		if (shader->enabled)
+			colors[i++] = shader->color_render;
+		shader = shader->next;
+	}
+}
+
 unsigned int		rt_render_ray(t_rt *rt, t_ray *ray)
 {
 	t_render		r;
@@ -65,8 +78,7 @@ unsigned int		rt_render_ray(t_rt *rt, t_ray *ray)
 
 	r = (t_render){
 		ray, rt, NULL,
-		HUGE_VAL,
-		0.0,
+		HUGE_VAL, 0.0,
 		(t_v4d){0.0, 0.0, 0.0, 0.0},
 		ray->dir,
 		(unsigned int*)colors
@@ -74,21 +86,9 @@ unsigned int		rt_render_ray(t_rt *rt, t_ray *ray)
 	ray->color = 0xff000000;
 	rt_node_foreach(rt->tree.bounded, INFIX, &rt_render_foreach, &r);
 	rt_node_foreach(rt->tree.unbounded, INFIX, &rt_render_foreach, &r);
-
-
 	if (r.obj_intersect)
 	{
-	t_shader		*shader = r.obj_intersect->shader->shader;
-	int				i;
-	i = 0;
-	while (shader)
-	{
-		if (shader->enabled)
-		{
-			colors[i++] = shader->color_render;
-		}
-		shader = shader->next;
-	}
+		init_colors((unsigned int*)colors, r.obj_intersect->shader->shader);
 		r.normal = r.obj_intersect->normal(r.obj_intersect, &(r.intersection));
 		r.ray->color = 0xFFFFFF00;
 		rt_node_foreach(rt->tree.light, INFIX, &rt_render_light, &r);
@@ -100,35 +100,7 @@ unsigned int		rt_render_ray(t_rt *rt, t_ray *ray)
 	return (rt_render_opacity(rt, ray, &r));
 }
 
-unsigned int		rt_render_bray(t_rt *rt, t_ray *ray)
-{
-	t_render	r;
-
-	r = (t_render){
-		ray, rt,
-		NULL,
-		HUGE_VAL,
-		0.0,
-		(t_v4d){0.0, 0.0, 0.0, 0.0},
-		ray->dir,
-		NULL
-	};
-	ray->color = 0xff000000;
-	rt_node_foreach(rt->tree.bounded, INFIX, &rt_render_foreach, &r);
-	rt_node_foreach(rt->tree.unbounded, INFIX, &rt_render_foreach, &r);
-	if (r.obj_intersect) {
-		r.ray->color = ((t_cube *)r.obj_intersect->content)->color;
-	}
-	else
-		return (get_background_color(&r));
-	ray->lenght = r.lowest_lenght;
-	return (rt_render_opacity(rt, ray, &r));
-}
-
 void				rt_render(t_rt *rt)
 {
-	if ((rt->settings.mode & MODE) == ALTERNATIVEMODE)
-		rt_init_alterate_rendermode(rt, &rt_rays);
-	else
-		rt_init_rt_rendermode(rt, &rt_rays);
+	rt_init_rt_rendermode(rt, &rt_rays);
 }
