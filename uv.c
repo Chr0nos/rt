3ba7bc52aa0bf45f0825aab9f35e97fe67bc57e4
Hantlowt <lhote.antoine@gmail.com>
2016-09-03 12:27:59 +0200

    Debut fix bug texture triangle

--- uv.c
+++ uv.c
@@ -6,7 +6,7 @@
 /*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/19 15:42:49 by alhote            #+#    #+#             */
-/*   Updated: 2016/08/20 12:52:01 by alhote           ###   ########.fr       */
+/*   Updated: 2016/09/03 12:14:59 by alhote           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,10 @@ t_v2f		get_uv_triangle(t_obj *obj, t_v4d p)
 {
 	t_v4d	bary;
 
-	bary = geo_barycentric_coordinates(((t_triangle*)(obj->content))->v1.pos,
-	((t_triangle*)(obj->content))->v2.pos,
-	((t_triangle*)(obj->content))->v3.pos, p);
+	bary = geo_barycentric_coordinates(geo_addv4(obj->trans.w,
+		((t_triangle*)(obj->content))->v1.pos),
+	geo_addv4(obj->trans.w, ((t_triangle*)(obj->content))->v2.pos),
+	geo_addv4(obj->trans.w, ((t_triangle*)(obj->content))->v3.pos), p);
 	return ((t_v2f){
 		(float)(bary.x * (double)((t_triangle*)(obj->content))->v1.uv.x +
 		bary.y * (double)((t_triangle*)(obj->content))->v2.uv.x + bary.z *
