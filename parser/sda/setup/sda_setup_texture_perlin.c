33c995d05a4005893099b7d31f6671cd0b222fbc
Sébastien Nicolet <snicolet@student.42.fr>
2016-09-09 04:06:50 +0200

    ergonomie: desormais set une texture sur un objet metra sa couleur en noir (pour que la texture ne soit pas teinte)

--- parser/sda/setup/sda_setup_texture_perlin.c
+++ parser/sda/setup/sda_setup_texture_perlin.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/29 14:33:16 by snicolet          #+#    #+#             */
-/*   Updated: 2016/09/09 03:59:07 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/09/09 04:01:40 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 
 int		sda_setup_texture_perlin(t_sda *e, t_obj *obj, char **av)
 {
-	(void)av;
 	rt_obj_set_texture(obj, texture_perlin(e->rt->textures,
 		(t_v2i){ft_atoi(av[0]), ft_atoi(av[1])},
 		ft_strunsplit((const char **)(unsigned long)av, ' ')));
