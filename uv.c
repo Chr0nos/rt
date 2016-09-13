eddfc728861ff192dd21cef9a4af280dcaa613da
Hantlowt <lhote.antoine@gmail.com>
2016-09-13 17:40:45 +0200

    update

--- uv.c
+++ uv.c
@@ -6,7 +6,7 @@
 /*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/19 15:42:49 by alhote            #+#    #+#             */
-/*   Updated: 2016/09/03 12:14:59 by alhote           ###   ########.fr       */
+/*   Updated: 2016/09/13 17:09:12 by alhote           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,21 @@
 t_v2f		get_uv_triangle(t_obj *obj, t_v4d p)
 {
 	t_v4d	bary;
+	t_v2f	result;
 
 	bary = geo_barycentric_coordinates(geo_addv4(obj->trans.w,
 		((t_triangle*)(obj->content))->v1.pos),
 	geo_addv4(obj->trans.w, ((t_triangle*)(obj->content))->v2.pos),
 	geo_addv4(obj->trans.w, ((t_triangle*)(obj->content))->v3.pos), p);
-	return ((t_v2f){
+	result = (t_v2f){
 		(float)(bary.x * (double)((t_triangle*)(obj->content))->v1.uv.x +
 		bary.y * (double)((t_triangle*)(obj->content))->v2.uv.x + bary.z *
 		(double)((t_triangle*)(obj->content))->v3.uv.x), (float)(bary.x *
 			(double)((t_triangle*)(obj->content))->v1.uv.y + bary.y *
 			(double)((t_triangle*)(obj->content))->v2.uv.y + bary.z *
 			(double)((t_triangle*)(obj->content))->v3.uv.y)
-	});
+	};
+	result.x = 1.0f - result.x;
+	result.y = 1.0f - result.y;
+	return (result);
 }
