12d46a481b1f49cb3ec033f6133aa4e113ae0db3
Sébastien Nicolet <snicolet@student.42.fr>
2018-12-28 02:14:00 +0100

    optimized uv.c

--- uv.c
+++ uv.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   uv.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alhote <alhote@student.42.fr>              +#+  +:+       +#+        */
+/*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/19 15:42:49 by alhote            #+#    #+#             */
-/*   Updated: 2016/09/14 19:16:14 by alhote           ###   ########.fr       */
+/*   Updated: 2018/12/28 02:11:59 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,22 @@
 
 t_v2f		get_uv_triangle(t_obj *obj, t_v4d p)
 {
-	t_v4d	bary;
-	t_v2f	result;
+	t_v4d					bary;
+	t_v2f					result;
+	const struct s_triangle	*tri = obj->content;
 
-	bary = geo_barycentric_coordinates(geo_addv4(obj->trans.w,
-		((t_triangle*)(obj->content))->v1.pos),
-	geo_addv4(obj->trans.w, ((t_triangle*)(obj->content))->v2.pos),
-	geo_addv4(obj->trans.w, ((t_triangle*)(obj->content))->v3.pos), p);
+	bary = geo_barycentric_coordinates(
+		geo_addv4(obj->trans.w, tri->v1.pos),
+		geo_addv4(obj->trans.w, tri->v2.pos),
+		geo_addv4(obj->trans.w, tri->v3.pos),
+		p);
 	result = (t_v2f){
-		(float)(bary.x * (double)((t_triangle*)(obj->content))->v1.uv.x +
-		bary.y * (double)((t_triangle*)(obj->content))->v2.uv.x + bary.z *
-		(double)((t_triangle*)(obj->content))->v3.uv.x), (float)(bary.x *
-			(double)((t_triangle*)(obj->content))->v1.uv.y + bary.y *
-			(double)((t_triangle*)(obj->content))->v2.uv.y + bary.z *
-			(double)((t_triangle*)(obj->content))->v3.uv.y)
+		(float)(bary.x * (double)tri->v1.uv.x +
+		bary.y * (double)tri->v2.uv.x + bary.z *
+		(double)tri->v3.uv.x), (float)(bary.x *
+			(double)tri->v1.uv.y + bary.y *
+			(double)tri->v2.uv.y + bary.z *
+			(double)tri->v3.uv.y)
 	};
 	result.y = 1.0f - result.y;
 	return (result);
