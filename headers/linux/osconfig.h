27cdae1a4f8fae78b67b3f64f32d5e82f3c19cda
Sébastien NICOLET <snicolet@student.42.fr>
2020-04-20 18:53:23 +0200

    fixed linux key codes

--- headers/linux/osconfig.h
+++ headers/linux/osconfig.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   osconfig.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
+/*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/11 18:50:54 by qloubier          #+#    #+#             */
-/*   Updated: 2016/06/21 11:56:40 by dboudy           ###   ########.fr       */
+/*   Updated: 2020/04/20 18:50:56 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 
 # include "draw.h"
 
-# define KC_W SDLK_z
+# define KC_W SDLK_w
+# define KC_A SDLK_a
 # define KC_S SDLK_s
+# define KC_D SDLK_d
 # define KC_ESCAPE SDLK_ESCAPE
 # define KC_SPACE SDLK_SPACE
 # define KC_E SDLK_e
-# define KC_Q SDLK_a
+# define KC_Q SDLK_q
 # define KC_LCTRL SDLK_LCTRL
 # define KC_C SDLK_c
-# define KC_A SDLK_q
-# define KC_D SDLK_d
 # define KC_UP SDLK_UP
 # define KC_DOWN SDLK_DOWN
 # define KC_LEFT SDLK_LEFT
